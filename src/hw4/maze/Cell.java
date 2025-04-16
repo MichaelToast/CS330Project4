816983e381228d1441fec0753f5d336a8c59e77c
MichaelToast <michaelp86788@gmail.com>
2025-04-16 11:30:36 -0500

    Made CellComponents an ENUM and added some requirement comments

--- src/hw4/maze/Cell.java
+++ src/hw4/maze/Cell.java
@@ -1,5 +1,27 @@
 package hw4.maze;
 
-public class Cell {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.params.provider.Arguments;
+
+public class Cell {
+	CellComponents exit;
+	CellComponents aperture;
+	CellComponents wall;
+	CellComponents wall2;
+	
+
+	// Constructor needs to follow: CellComponents.WALL, CellComponents.EXIT, CellComponents.APERTURE, CellComponents.WALL
+	public Cell(CellComponents wall, CellComponents exit, CellComponents aperture, CellComponents wall2) {
+		this.exit = exit;
+		this.aperture = aperture;
+		this.wall = wall;
+		this.wall2 = wall2;
+	}
+	
+	// Need methods of: getLeft(), getRight(), getUp(), getDown()
+	
+	// To string needs to result in: assertEquals("Cell [left=" + cell.getLeft() + ", right=" + cell.getRight() + ", up=" + cell.getUp() + ", down=" + cell.getDown() + "]", cell.toString());
+
+	
 }
--- src/hw4/maze/Grid.java
+++ src/hw4/maze/Grid.java
@@ -1,5 +1,15 @@
 package hw4.maze;
 
+import java.util.ArrayList;
+
 public class Grid {
+	ArrayList<Row> rows;
+
+	public Grid(ArrayList<Row> rowList) {
+		this.rows = rowList;
+	}
+	
+	
+	// Need methods of: getRows(), setRows()
 
 }

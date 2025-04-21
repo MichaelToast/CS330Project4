513cdcd11d9c8c1ccf90049459088d8fac93984f
Michael Huber <128461719+GitAtMike@users.noreply.github.com>
2025-04-21 13:22:53 -0500

    Set Up Cell and CellComponents

--- src/hw4/game/Game.java
+++ src/hw4/game/Game.java
@@ -1,5 +1,36 @@
 package hw4.game;
 
-public class Game {
+import hw4.maze.Grid;
+import hw4.player.Player;
+import hw4.player.Movement;
 
+public class Game {
+    private Grid grid;
+
+    public Game(Grid grid) {
+        this.grid = grid;
+    }
+    
+    public Game(int size){
+        if (size > 3 && size <= 7){
+            this.grid = createRandomGrid(size);
+        }
+        else{
+            this.grid = null;
+        }
+    }
+
+    public Grid getGrid() {
+        return grid;
+    }
+
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
+
+    public boolean play(Movement movement) {
+
+    }
+
+    
 }
--- src/hw4/maze/Cell.java
+++ src/hw4/maze/Cell.java
@@ -1,5 +1,56 @@
 package hw4.maze;
 
 public class Cell {
+    private CellComponents left, right, up, down;
 
+    public Cell(CellComponents left, CellComponents right, CellComponents up, CellComponents down) {
+        this.left = (left != null) ? left : CellComponents.WALL;
+        this.right = (right != null) ? right : CellComponents.WALL;
+        this.up = (up != null) ? up : CellComponents.WALL;
+        this.down = (down != null) ? down : CellComponents.WALL;
+    }
+
+    public CellComponents getLeft() {
+        return left;
+    }
+    
+    public CellComponents getRight() {
+        return right;
+    }
+    
+    public CellComponents getUp() {
+        return up;
+    }
+    
+    public CellComponents getDown() {
+        return down;
+    }
+
+    public void setLeft(CellComponents left) {
+        if (left != null) {
+            this.left = left;
+        }
+    }
+    
+    public void setRight(CellComponents right) {
+        if (right != null) {
+            this.right = right;
+        }
+    }
+
+    public void setUp(CellComponents up) {
+        if (up != null) {
+            this.up = up;
+        }
+    }
+    
+    public void setDown(CellComponents down) {
+        if (down != null) {
+            this.down = down;
+        }
+    }
+
+    public String toString() {
+        return "Cell [left=" + left + ", right=" + right + ", up=" + up + ", down=" + down + "]";
+    }
 }

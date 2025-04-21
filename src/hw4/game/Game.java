0b06bb5a0b2b19636f17745c8cc5c172152d4bf3
Michael Huber <128461719+GitAtMike@users.noreply.github.com>
2025-04-21 14:23:59 -0500

    Completed Play Style and Broke Down Grid Set Up

--- src/hw4/game/Game.java
+++ src/hw4/game/Game.java
@@ -33,79 +33,166 @@ public class Game {
     public void setGrid(Grid grid) {
         this.grid = grid;
     }
-
-    public Grid createRandomGrid(int size) {
-        if(size < 3 || size > 7){
-            return null;
+///////////////////////////////////////////////////
+    public  boolean play(Player player, Movement movement) {
+        if (movement == null || player  == null) {
+            return false;
         }
-        
-        Random rand = new Random();
-
-        Cell[][] cells = new Cell[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                cells[i][j] = new Cell(null, null, null, null);
-            }
+        switch(movement){
+            case UP: return moveUp(player);
+            case RIGHT: return moveRight(player);
+            case DOWN: return moveDown(player);
+            case LEFT: return moveLeft(player);
+            default: return false;
         }
-        
-        int exitRow = rand.nextInt(size);
-        cells[exitRow][0].setLeft(CellComponents.EXIT);
-
-        for(int i = 0; i < size; i++) {
-            for(int j = 1; j < size; j++) {
-                Cell currCell = cells[i][j];
-
-                if(i > 0) {
-                    Cell above = cells[i - 1][j];
-                    currCell.setUp(above.getDown());
-                }
-                else{
-                    currCell.setUp(randomComponent(rand));
-                }
-
-                if(j > 0){
-                    Cell left = cells[i][j - 1];
-                    currCell.setLeft(left.getRight());
-                }
-                else if(i != exitRow){
-                    currCell.setLeft(randomComponent(rand));
-                }
-
-                if(j < size - 1){
-                    currCell.setRight(randomComponent(rand));
-                }
-
-                if(i < size - 1){
-                    currCell.setDown(randomComponent(rand));
-                }
-
-                if(!hasAtLeastOneAperture(currCell)){
-                    currCell.setRight(CellComponents.APERTURE);
-                }
-            }
-        }
-
-        ArrayList<Row> rows = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            ArrayList<Cell> cellsInRow = new ArrayList<>();
-            for (int j = 0; j < size; j++) {
-                cellsInRow.add(cells[i][j]);
-            }
-            rows.add(new Row(cellsInRow));
-        }
-
-        return new Grid(rows);
     }
 
-    private CellComponents randomComponent(Random rand) {
-        return rand.nextBoolean() ? CellComponents.APERTURE : CellComponents.WALL;
+    private boolean moveUp(Player player) {
+        int rowIndex = getRowIndex(player);
+        int columnIndex = getColumnIndex(player);
+        if(rowIndex > 0 && player..getCurrCell().getUp() == CellComponents.APERTURE){
+            updatePosition(player, rowIndex - 1, columnIndex);
+        }
     }
 
-    private boolean hasAtLeastOneAperture(Cell cell) {
-        return cell.getLeft() == CellComponents.APERTURE || 
-            cell.getRight() == CellComponents.APERTURE ||  
-            cell.getUp() == CellComponents.APERTURE || 
-            cell.getDown() == CellComponents.APERTURE;
+    private boolean moveRight(Player player) {
+        int rowIndex = getRowIndex(player);
+        int columnIndex = getColumnIndex(player);
+        if(columnIndex < grid.getRows().get(0).getCells().size() - 1 && player..getCurrCell().getRight() == CellComponents.APERTURE){
+            updatePosition(player, rowIndex, columnIndex + 1);
+        }
     }
+
+    private boolean moveDown(Player player) {
+        int rowIndex = getRowIndex(player);
+        int columnIndex = getColumnIndex(player);
+        if(rowIndex < grid.getRows().size() - 1 && player..getCurrCell().getDown() == CellComponents.APERTURE){
+            updatePosition(player, rowIndex + 1, columnIndex);
+        }
+    }
+
+    private boolean moveLeft(Player player) {
+        int rowIndex = getRowIndex(player);
+        int columnIndex = getColumnIndex(player);
+        if(columnIndex > 0 && player..getCurrCell().getLeft() == CellComponents.APERTURE){
+            updatePosition(player, rowIndex, columnIndex - 1);
+        }
+    }
+
+    private int getRowIndex(Player player) {
+        return  grid.getRows().indexOf(player.getCurrentRow());
+    }
+
+    private int getColumnIndex(Player player) {
+        return player.getCurrentRow().getCells().indexOf(player.getCurrentCell());        
+    }
+
+    private void updatePosition(Player player, int row, int column){
+        Row currRow = grid.getRows().get(row);
+        player.setCurrentRow(currRow);
+        player.setCurrentCell(currRow.getCells().get(column));
+    }
+///////////////////////////////////////////////////
+public Grid createRandomGrid(int size) {
+    if (size < 3 || size > 7) return null;
+
+    Cell[][] cells = initializeEmptyCells(size);
+    placeExit(cells);
+    populateCellComponents(cells);
+    ensureEachCellHasAperture(cells);
+
+    return convertToGrid(cells);
+}
+
+private Cell[][] initializeEmptyCells(int size) {
+    Cell[][] cells = new Cell[size][size];
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cells[i][j] = new Cell(null, null, null, null);
+        }
+    }
+    return cells;
+}
+
+private void placeExit(Cell[][] cells) {
+    Random rand = new Random();
+    int exitRow = rand.nextInt(cells.length);
+    cells[exitRow][0].setLeft(CellComponents.EXIT);
+}
+
+private void populateCellComponents(Cell[][] cells) {
+    Random rand = new Random();
+    int size = cells.length;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            Cell current = cells[i][j];
+
+            // UP
+            if (i > 0) {
+                current.setUp(cells[i - 1][j].getDown());
+            } else {
+                current.setUp(randomComponent(rand));
+            }
+
+            // LEFT
+            if (j > 0) {
+                current.setLeft(cells[i][j - 1].getRight());
+            } else if (current.getLeft() != CellComponents.EXIT) {
+                current.setLeft(randomComponent(rand));
+            }
+
+            // RIGHT
+            if (j < size - 1) {
+                current.setRight(randomComponent(rand));
+            }
+
+            // DOWN
+            if (i < size - 1) {
+                current.setDown(randomComponent(rand));
+            }
+        }
+    }
+}
+
+private void ensureEachCellHasAperture(Cell[][] cells) {
+    for (int i = 0; i < cells.length; i++) {
+        for (int j = 0; j < cells[i].length; j++) {
+            Cell cell = cells[i][j];
+            if (!hasAtLeastOneAperture(cell)) {
+                // Preferably adjust RIGHT if possible
+                if (j < cells[i].length - 1) {
+                    cell.setRight(CellComponents.APERTURE);
+                } else {
+                    cell.setLeft(CellComponents.APERTURE);
+                }
+            }
+        }
+    }
+}
+
+private Grid convertToGrid(Cell[][] cells) {
+    ArrayList<Row> rows = new ArrayList<>();
+    for (int i = 0; i < cells.length; i++) {
+        ArrayList<Cell> rowCells = new ArrayList<>();
+        for (int j = 0; j < cells[i].length; j++) {
+            rowCells.add(cells[i][j]);
+        }
+        rows.add(new Row(rowCells));
+    }
+    return new Grid(rows);
+}
+
+
+private CellComponents randomComponent(Random rand) {
+    return rand.nextBoolean() ? CellComponents.APERTURE : CellComponents.WALL;
+}
     
+private boolean hasAtLeastOneAperture(Cell cell) {
+    return cell.getLeft() == CellComponents.APERTURE ||
+            cell.getRight() == CellComponents.APERTURE ||
+            cell.getUp() == CellComponents.APERTURE ||
+            cell.getDown() == CellComponents.APERTURE;
+}
+
 }
